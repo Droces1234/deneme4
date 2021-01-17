91c27559540a9ab359247188d91d3d13ef2e2cb4
Droces1234 <ebubekircicek1234@gmail.com>
2021-01-17 16:50:27 +0300

    project pertama

---
+++ Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formlar_arası_geçiş
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form1 yeni2 = new Form1();
+            yeni2.Show();
+            this.Hide();
+        }
+    }
+}
